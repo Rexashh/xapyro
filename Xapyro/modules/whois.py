eaf5d94d39b85d3b996f763091ae8d3022270fc6
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-06-15 09:28:17 +0700

    Update whois.py

--- Xapyro/modules/whois.py
+++ Xapyro/modules/whois.py
@@ -13,7 +13,7 @@ from pyrogram import filters
 from pyrogram.types import User, InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.raw import functions
 from pyrogram.errors import PeerIdInvalid
-from xapyro import app
+from Xapyro import app
 from config import PREFIX
 
 
