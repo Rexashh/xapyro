2f732cc7b67e2780618167e5e114fb0f4925fc2c
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-06-30 20:13:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,3 @@ PREFIX = os.getenv("PREFIX")
 LOG_CHAT = int(os.getenv("LOG_CHAT"))
 HEROKU_API = os.getenv("HEROKU_API")
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
