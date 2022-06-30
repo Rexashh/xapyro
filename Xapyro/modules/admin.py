a9b5fefbe59b93d69cf3dd5bdc7ed0c69c131f64
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-06-30 17:08:48 +0700

    try

--- Xapyro/modules/admin.py
+++ Xapyro/modules/admin.py
@@ -90,8 +90,6 @@ async def unban(_, message: Message):
 mute_permission = ChatPermissions(
     can_send_messages=False,
     can_send_media_messages=False,
-    can_send_animations=False,
-    can_send_games=False,
     can_use_inline_bots=False,
     can_add_web_page_previews=False,
     can_send_polls=False,
@@ -130,8 +128,6 @@ async def mute_hammer(_, message: Message):
 unmute_permissions = ChatPermissions(
     can_send_messages=True,
     can_send_media_messages=True,
-    can_send_animations=True,
-    can_send_games=True,
     can_use_inline_bots=True,
     can_add_web_page_previews=True,
     can_send_polls=True,
